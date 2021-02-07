179b65b5f72bc8df4fdeeda05b67a4172f9b3825
paulina bystrowska <bystrowska@users.noreply.github.com>
2021-02-07 15:59:04 +0000

    add sample text and configure sass plus layout

---
+++ src/components/layout.js
@@ -0,0 +1,7 @@
+import React from "react"
+import "fontsource-pt-mono" // Defaults to weight 400 with all styles included.
+import "./layout.scss"
+
+export default function Layout({ children }) {
+  return <div>{children}</div>
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,149 +1,24 @@
 import * as React from "react"
-
-// styles
-const pageStyles = {
-  color: "#232129",
-  padding: "96px",
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
-const headingAccentStyles = {
-  color: "#663399",
-}
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
-const listStyles = {
-  marginBottom: 96,
-  paddingLeft: 0,
-}
-const listItemStyles = {
-  fontWeight: "300",
-  fontSize: "24px",
-  maxWidth: "560px",
-}
-
-const linkStyle = {
-  color: "#8954A8",
-  fontWeight: "bold",
-  fontSize: "16px",
-  verticalAlign: "5%",
-}
-
-const docLinkStyle = {
-  ...linkStyle,
-  listStyleType: "none",
-  marginBottom: 24,
-}
-
-const descriptionStyle = {
-  color: "#232129",
-  fontSize: "14px",
-}
-
-const docLink = {
-  text: "Documentation",
-  url: "https://www.gatsbyjs.com/docs/",
-  color: "#8954A8",
-}
-// data
-const links = [
-  {
-    text: "Tutorial",
-    url: "https://www.gatsbyjs.com/docs/tutorial/",
-    description:
-      "A great place to get started if you're new to web development. Designed to guide you through setting up your first Gatsby site.",
-    color: "#E95800",
-  },
-  {
-    text: "How to Guides",
-    url: "https://www.gatsbyjs.com/docs/how-to/",
-    description:
-      "Practical step-by-step guides to help you achieve a specific goal. Most useful when you're trying to get something done.",
-    color: "#1099A8",
-  },
-  {
-    text: "Reference Guides",
-    url: "https://www.gatsbyjs.com/docs/reference/",
-    description:
-      "Nitty-gritty technical descriptions of how Gatsby works. Most useful when you need detailed information about Gatsby's APIs.",
-    color: "#BC027F",
-  },
-  {
-    text: "Conceptual Guides",
-    url: "https://www.gatsbyjs.com/docs/conceptual/",
-    description:
-      "Big-picture explanations of higher-level Gatsby concepts. Most useful for building understanding of a particular topic.",
-    color: "#0D96F2",
-  },
-  {
-    text: "Plugin Library",
-    url: "https://www.gatsbyjs.com/plugins",
-    description:
-      "Add functionality and customize your Gatsby site or app with thousands of plugins built by our amazing developer community.",
-    color: "#000000",
-  },
-]
+import Layout from "../components/layout"
 
 // markup
 const IndexPage = () => {
   return (
-    <main style={pageStyles}>
-      <title>Home Page</title>
-      <h1 style={headingStyles}>
-        Congratulations Paulina
-        <br />
-        <span style={headingAccentStyles}>— you just made a Gatsby site! </span>
-        <span role="img" aria-label="Party popper emojis">
-          🎉🎉🎉
-        </span>
-      </h1>
-      <p style={paragraphStyles}>
-        Edit <code style={codeStyles}>src/pages/index.js</code> to see this page
-        update in real-time.{" "}
-        <span role="img" aria-label="Sunglasses smiley emoji">
-          😎
-        </span>
+    <Layout>
+      <h1>hi.</h1>
+      <p>
+        <span class="pronoun">I</span> <span class="verb">am</span> Paulina. <span class="pronoun">I</span> <span class="verb">have finished</span> my <span class="number">2nd</span> <span class="noun">year</span> of <span class="noun">BSc</span> in <span class="noun">Computer Science</span> at <span class="noun">University</span> of <span class="noun">Leeds</span> and <span class="pronoun">I</span> <span class="verb">am</span> currently <span class="verb">doing</span> a <span class="noun">placement year</span>, <span class="verb">working</span> as <span class="noun">Software Engineer</span> at <span class="noun">Bloomberg LP</span> as <span class="noun">part</span> of my <span class="noun">degree</span>. <span class="pronoun">I</span> <span class="verb">will be graduating</span> in <span class="noun">Jun</span> <span class="number">2022</span> and <span class="verb">plan</span> to <span class="verb">start working</span> towards a <span class="noun">Master’s Degree</span>.
       </p>
-      <ul style={listStyles}>
-        <li style={docLinkStyle}>
-          <a
-            style={linkStyle}
-            href={`${docLink.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter`}
-          >
-            {docLink.text}
-          </a>
-        </li>
-        {links.map(link => (
-          <li style={{ ...listItemStyles, color: link.color }}>
-            <span>
-              <a
-                style={linkStyle}
-                href={`${link.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter`}
-              >
-                {link.text}
-              </a>
-              <p style={descriptionStyle}>{link.description}</p>
-            </span>
-          </li>
-        ))}
-      </ul>
-      <img
-        alt="Gatsby G Logo"
-        src="data:image/svg+xml,%3Csvg width='24' height='24' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M12 2a10 10 0 110 20 10 10 0 010-20zm0 2c-3.73 0-6.86 2.55-7.75 6L14 19.75c3.45-.89 6-4.02 6-7.75h-5.25v1.5h3.45a6.37 6.37 0 01-3.89 4.44L6.06 9.69C7 7.31 9.3 5.63 12 5.63c2.13 0 4 1.04 5.18 2.65l1.23-1.06A7.959 7.959 0 0012 4zm-8 8a8 8 0 008 8c.04 0 .09 0-8-8z' fill='%23639'/%3E%3C/svg%3E"
-      />
-    </main>
+      <p>
+        I have professional experience working with C++ on time sensitive distributed system processing up to 500,000 packets of data per day. As well as Python and TypeScript.
+      </p>
+      <p>
+        I’m interested in Machine Learning and in particular Natural Language Processing and how ML can help us both better understand human communication and linguistics as well as how we can use it to generate content.
+      </p>
+      <p>
+        I love expressing myself creatively, this website for example went through multiple iterations of designs and took far longer to design than to code. And in 2021 I’m committed to making at least one digital painting per month. When I need a break from staring at a screen I go bouldering. I love the feeling of adrenaline and accomplishment I experience after completing a particularly hard bouldering problem. It’s similar to satisfaction I feel after getting a programme to run or finally fixing a bug. I think that’s what draws me to both of those things.
+      </p>
+    </Layout>
   )
 }
 
